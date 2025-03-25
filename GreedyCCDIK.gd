44c71f72b2cb89ccddb3b89b2f4a5ce53fa548c0
Gabbagen <gabbagen20@gmail.com>
2025-03-26 00:21:01 +0300

    refactored out like 80%

--- GreedyCCDIK.gd
+++ GreedyCCDIK.gd
@@ -2,29 +2,6 @@ extends IKSolver
 class_name GreedyCCDIK
 
 
-var cache : Array[float]
-
-# TODO clean up draw solve from solve
-func solve() -> Array[float]:
-	var iterations : int = 0
-	var start = Time.get_unix_time_from_system()
-	fill_cache()
-	goal_position = get_viewport().get_mouse_position()
-	while not cache_goal_reached(): 
-		cache = step()
-		iterations += 1
-	debug_label.text = str(iterations) + " iterations to solve \n"
-	debug_label.text += str((Time.get_unix_time_from_system() - start) * 1000) + " ms spent"
-	return cache
-
-
-func draw_step():
-	fill_cache()
-	goal_position = get_viewport().get_mouse_position()
-	set_pose(step())
-	redraw_constraints()
-
-
 func step() -> Array[float]:
 	var output : Array[float] = cache.duplicate()
 	
@@ -48,69 +25,3 @@ func choose_segment(cache : Array[float]) -> int:
 			best_segment = i
 	print(best_distance)
 	return best_segment
-
-
-func count_cached_distance(cache : Array[float], i : int) -> float:
-	var endpoint = get_cached_end_position(cache, segments.size() - 1)
-	var position = get_cached_transform(cache, i).origin
-	return abs(position.distance_to(goal_position) - position.distance_to(endpoint))
-
-
-func fill_cache():
-	cache.clear()
-	for segment in segments:
-		cache.append(segment.rotation)
-
-
-func get_cached_transform(output : Array[float], segment : int) -> Transform2D:
-	var transform : Transform2D = Transform2D(output[0], segments[0].global_position)
-	for i in range(1, segment + 1):
-		transform = transform * Transform2D(output[i], segments[i].position)
-	return transform
-
-func get_cached_end_position(output : Array[float], segment : int) -> Vector2:
-	var transform : Transform2D = Transform2D(output[0], segments[0].global_position)
-	for i in range(1, segment + 1):
-		transform = transform * Transform2D(output[i], segments[i].position)
-	return transform * Vector2(segments[segments.size() - 1].length, 0)
-
-
-# if our "imagined" angles configuration returns a valuable solution for IK chain
-# TODO unreachable
-func cache_goal_reached() -> bool:
-	return get_cached_end_position(cache, segments.size() - 1).distance_to(goal_position) < tolerance
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# asdsdgas
--- IKSolver.gd
+++ IKSolver.gd
@@ -10,6 +10,8 @@ var segments : Array[ArmSegment]
 
 var goal_position : Vector2
 
+var cache : Array[float]
+
 func _ready():
 	segments.append(first_segment)
 	build_chain(first_segment)
@@ -23,11 +25,29 @@ func build_chain(segment : ArmSegment):
 
 
 func draw_solve():
+	fill_cache()
+	goal_position = get_viewport().get_mouse_position()
 	set_pose(solve())
 	redraw_constraints()
 
+
+# TODO clean up draw solve from solve
+func solve() -> Array[float]:
+	var start = Time.get_unix_time_from_system()
+	var iterations : int = 0
+	while not cache_goal_reached(): 
+		cache = step()
+		iterations += 1
+	debug_label.text = str(iterations) + " iterations to solve \n"
+	debug_label.text += str((Time.get_unix_time_from_system() - start) * 1000) + " ms spent"
+	return cache
+
+
 func draw_step():
-	pass
+	fill_cache()
+	goal_position = get_viewport().get_mouse_position()
+	set_pose(step())
+	redraw_constraints()
 
 
 func set_pose(angles : Array[float]):
@@ -39,25 +59,41 @@ func step() -> Array[float]:
 	return []
 
 
-func solve() -> Array[float]:
-	return []
+func fill_cache():
+	cache.clear()
+	for segment in segments:
+		cache.append(segment.rotation)
 
 
 func redraw_constraints():
 	for segment in segments:
 		segment.redraw_constraints()
 
+
 # if our "imagined" angles configuration returns a valuable solution for IK chain
-## TODO unreachable
-#func cache_goal_reached() -> bool:
-	## TODO imagine cache
-	#return segments[segments.size() - 1].end.global_position.distance_to(goal_position) < tolerance
+# TODO unreachable
+func cache_goal_reached() -> bool:
+	return get_cached_end_position(cache, segments.size() - 1).distance_to(goal_position) < tolerance
 
 
+func get_cached_transform(output : Array[float], segment : int) -> Transform2D:
+	var transform : Transform2D = Transform2D(output[0], segments[0].global_position)
+	for i in range(1, segment + 1):
+		transform = transform * Transform2D(output[i], segments[i].position)
+	return transform
 
 
+func get_cached_end_position(output : Array[float], segment : int) -> Vector2:
+	var transform : Transform2D = Transform2D(output[0], segments[0].global_position)
+	for i in range(1, segment + 1):
+		transform = transform * Transform2D(output[i], segments[i].position)
+	return transform * Vector2(segments[segment].length, 0)
 
 
+func count_cached_distance(cache : Array[float], i : int) -> float:
+	var endpoint = get_cached_end_position(cache, segments.size() - 1)
+	var position = get_cached_transform(cache, i).origin
+	return abs(position.distance_to(goal_position) - position.distance_to(endpoint))
 
 
 
